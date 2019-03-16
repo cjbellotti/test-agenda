41870cff961c9ef0da964b4c06775a066496d777
Carlos Bellotti <cjbellotti@hotmail.com>
2019-03-16 13:39:55 -0300

    Agrego bootstrap a la aplicacion

--- test-agenda-front-ciruja/app.js
+++ test-agenda-front-ciruja/app.js
@@ -9,22 +9,25 @@ $('#refresh').click(function (e) {
     e.stopPropagation();
     load();
 });
+
+var currentRecord = null;
+
 function save() {
-    var nombre = $('[name="nombre"]').val();
-    var apellido = $('[name="apellido"]').val();
-    var observaciones = $('[name="observaciones"]').val();
+    if (currentRecord == null) {
+        currentRecord = {};
+    }
+    currentRecord.nombre = $('[name="nombre"]').val();
+    currentRecord.apellido = $('[name="apellido"]').val();
+    currentRecord.observaciones = $('[name="observaciones"]').val();
 
     $.ajax({
         type : "POST",
         url : "http://localhost:8080/personas",
         contentType : 'application/json',
-        data : JSON.stringify({
-            "nombre" : nombre,
-            "apellido" : apellido,
-            "observaciones" : observaciones
-        }),
+        data : JSON.stringify(currentRecord),
         success : function () {
             alert("Persona guardada correctamente.");
+            currentId = null;
             load();
         },
         error : function() {
@@ -38,18 +41,50 @@ function load() {
         type : 'GET',
         url : 'http://localhost:8080/personas',
         success : function (respuesta) {
-            var ul = $('#lista');
-            ul.html('');
-            respuesta.forEach(function (item) {
+            var lista = $('#lista');
+            lista.html('');
+            // respuesta.forEach(function (item) {
+            //     console.log(item)
+            //     var li = $(`
+            //                 <li>
+            //                     ${item.nombre} <button onclick="modificar(${item.id})">Modificar</button>
+            //                 </li>
+            //                 `);
+            //     lista.append(li);
+            // })
+
+            for (var i = 0; i < respuesta.length; i++) {
+                var item = respuesta[i];
                 console.log(item)
-                var li = $('<li></li>').html(item.nombre);
-                ul.append(li);
-            })
+                var li = $(`
+                            <li>
+                                ${i}. ${item.nombre} <button onclick="modificar(${item.id})">Modificar</button>
+                            </li>
+                            `);
+                lista.append(li);
+            }
         },
         error : function() {
             alert("Ocurrió un error");
         }
     });
+}
+
+function modificar(id) {
+    $.ajax({
+        method : 'GET',
+        url : 'http://localhost:8080/personas/' + id,
+        success : function (response) {
+            console.log(response);
+            currentRecord = response;
+            $('[name=nombre]').val(response.nombre);
+            $('[name=apellido]').val(response.apellido);
+            $('[name=observaciones]').val(response.observaciones);
+        },
+        error : function() {
+            alert('Error al cargar la persona')
+        }
+    })
 }
 
 $(document).ready(() => {
