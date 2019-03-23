f01ac6b78e8a0c1650779912ea275c37446ca5f3
Carlos Bellotti <cjbellotti@hotmail.com>
2019-03-23 11:46:59 -0300

    Nuevo repaso de front

--- test-agenda-front-ciruja/app.js
+++ test-agenda-front-ciruja/app.js
@@ -1,93 +1,67 @@
-$('#enviar').click(function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    save();
-});
-
-$('#refresh').click(function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    load();
-});
-
-var currentRecord = null;
+var currentRecord = {}
 
 function save() {
-    if (currentRecord == null) {
-        currentRecord = {};
-    }
-    currentRecord.nombre = $('[name="nombre"]').val();
-    currentRecord.apellido = $('[name="apellido"]').val();
-    currentRecord.observaciones = $('[name="observaciones"]').val();
+    currentRecord.nombre = $('#nombre').val();
+    currentRecord.apellido = $('#apellido').val();
+    currentRecord.observaciones = $('#observaciones').val(); 
 
     $.ajax({
-        type : "POST",
-        url : "http://localhost:8080/personas",
+        method : 'POST',
+        url : 'http://localhost:8080/personas',
         contentType : 'application/json',
         data : JSON.stringify(currentRecord),
-        success : function () {
-            alert("Persona guardada correctamente.");
-            currentId = null;
+        success : () => {
+            currentRecord = {};
+            alert('Se guardo la persona correctamente.');
+            $('#nombre').val('');
+            $('#apellido').val('');
+            $('#observaciones').val('');
+            $('#nombre').focus()
             load();
         },
-        error : function() {
-            alert("Ocurrió un error");
+        error : () => {
+            alert('Ha ocurrido un error');
         }
     })
 }
 
 function load() {
-    $.ajax({
-        type : 'GET',
-        url : 'http://localhost:8080/personas',
-        success : function (respuesta) {
-            var lista = $('#lista');
-            lista.html('');
-            // respuesta.forEach(function (item) {
-            //     console.log(item)
-            //     var li = $(`
-            //                 <li>
-            //                     ${item.nombre} <button onclick="modificar(${item.id})">Modificar</button>
-            //                 </li>
-            //                 `);
-            //     lista.append(li);
-            // })
 
-            for (var i = 0; i < respuesta.length; i++) {
-                var item = respuesta[i];
-                console.log(item)
-                var li = $(`
-                            <li>
-                                ${i}. ${item.nombre} <button onclick="modificar(${item.id})">Modificar</button>
-                            </li>
-                            `);
-                lista.append(li);
-            }
-        },
-        error : function() {
-            alert("Ocurrió un error");
-        }
-    });
-}
-
-function modificar(id) {
+    var lista = $('#lista');
+    lista.html('');
     $.ajax({
         method : 'GET',
-        url : 'http://localhost:8080/personas/' + id,
-        success : function (response) {
-            console.log(response);
-            currentRecord = response;
-            $('[name=nombre]').val(response.nombre);
-            $('[name=apellido]').val(response.apellido);
-            $('[name=observaciones]').val(response.observaciones);
+        url : 'http://localhost:8888/personas',
+        success : (response) => {
+            response.forEach((item) => {
+                lista.append($(`<div class="lista-item d-flex justify-content-between"><span>${item.nombre} ${item.apellido}</span><button class="btn btn-primary" onclick="loadPersona(${item.id})">Ver</button></div>`));
+            });
         },
-        error : function() {
-            alert('Error al cargar la persona')
+        error : () => {
+            alert('Ha ocurrido un error')
+        }
+    })
+
+}
+
+function loadPersona(id) {
+
+    $.ajax({
+        method : 'GET',
+        url : 'http://localhost:8888/personas/' + id,
+        success : function (response) {
+            currentRecord = response;
+            $('#nombre').val(response.nombre);
+            $('#apellido').val(response.apellido);
+            $('#observaciones').val(response.observaciones);
+        },
+        error : function () {
+            alert('Ha ocurrido un error al leer la persona');
         }
     })
 }
 
-$(document).ready(() => {
-    console.log('Iniciando....')
-    load();
-});
+$(() => {
+ load();   
+})
+
